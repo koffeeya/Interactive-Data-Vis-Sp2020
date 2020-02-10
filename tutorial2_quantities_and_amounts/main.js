31e04b67009f583727c6015b29441757c3673f2e
koffeeya <kavya.beheraj@gmail.com>
2020-02-09 19:21:50 -0500

    started tutorial 2

---
+++ tutorial2_quantities_and_amounts/main.js
@@ -0,0 +1,14 @@
+// load in the data
+d3.csv("../data/squirrelActivities.csv", d3.autoType).then(data => {
+    console.log(data);
+
+    // CONSTANTS
+    
+
+    // SCALES
+
+
+    // MAIN CODE
+
+
+});
