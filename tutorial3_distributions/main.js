1aa267f78eb8511278e1015dc8a6d6a7f17c639c
koffeeya <kavya.beheraj@gmail.com>
2020-02-20 00:03:53 -0500

    tutorial 3

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -1,5 +1,5 @@
-/* CONSTANTS & GLOBALS */
-// variables we will access or change in the rest of the code
+/* CONSTANTS & GLOBALS
+variables we will access or change in the rest of the code */
 
 // Constants
 const width = window.innerWidth * 0.7,
@@ -13,44 +13,194 @@ const width = window.innerWidth * 0.7,
   radius = 5;
 
 // Globals
+let svg;
+let xScale;
+let yScale;
+let colorScale;
 
 
-
-/* APPLICATION STATE */
-// variable to store current state of data
+/* APPLICATION STATE
+variable to store current state of data */
 let state = {
-
+  data: [],
+  selectedGenre: "All"
 };
 
 
-/* LOAD DATA */
-// load data and run the init() function
-d3.json().then({
-
+/* LOAD DATA 
+load data and run the init() function */
+d3.csv("../../data/gameRatings.csv", d3.autoType).then(raw_data => {
+  console.log("loaded data!");
+  state.data = raw_data;
+  init()
+  console.log("initialized!")
 });
 
 
-/* INITIALIZING FUNCTION */
-// runs one time to set up scales, axes, and UI
+/* INITIALIZING FUNCTION 
+runs one time to set up scales, axes, and UI */
 function init() {
+  console.log(d3.map(state.data, d => d.genre).keys().push("All"))
 
   // Scales
+  xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(state.data, d => d.user_score))
+    .range([margin.left, width - margin.right]);
+
+  yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(state.data, d => d.critic_score))
+    .range([height - margin.bottom, margin.top]);
+
+  colorScale = d3
+    .scaleSequential(d3.interpolateRdBu)
+    .domain(d3.extent(state.data, d => d.user_diff))
+
 
   // Axes
+  const xAxis = (d3.axisBottom(xScale));
+  const yAxis = (d3.axisLeft(yScale));
+  
 
-  // UI setup
+  // UI element setup
+  const selectElement = d3.select("#dropdown").on("change", function () {
+    console.log("The new selected genre is", this.value);
+    state.selectedGenre = this.value;
+    draw();
+  });
+
+  const div = d3
+  .select('#d3-container')
+  .append('div')
+  .attr('class', 'tooltip')
+  .style('opacity', 0);
+
+
+  // add in dropdown options from data
+  selectElement
+    .selectAll("option")
+    .data(d3.map(state.data, d => d.genre).keys().sort().concat(["All"]))
+    .join("option")
+    .attr("value", d => d)
+    .text(d => d);
+
+  // SVG setup
+  svg = d3
+    .select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg
+    .append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    .call(xAxis)
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", "50%")
+    .attr("dy", "3em")
+    .text("User Rating");
+
+  svg
+    .append("g")
+    .attr("class", "axis y-axis")
+    .attr("transform", `translate(0, ${margin.left})`)
+    .call(yAxis)
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("y", "50%")
+    .attr("dx", "-3em")
+    .attr("writing-mode", "vertical-rl")
+    .text("Critic Rating");
 
   // Draw
-
+  draw()
+  console.log("data redrawn!")
 }
 
 
-/* DRAW FUNCTION */
-// called every time the data/state is updated
+/* DRAW FUNCTION
+called every time the data/state is updated */
 function draw() {
 
   // Filters
+  let filteredData = state.data;
+
+  if (state.selectedGenre !== "All") {
+    filteredData = state.data.filter(d => d.genre === state.selectedGenre);
+  }
+
+  const div = d3
+  .select('body')
+  .append('div')
+  .attr('class', 'tooltip')
+  .style('opacity', 0);
 
   // SVG: Enter, update, and exit
+  const dot = svg
+    .selectAll(".dot")
+    .data(filteredData, d => d.name)
+    .join(
 
+      // enter
+      enter => enter
+      .append("circle")
+      .attr("class", "dot")
+      .attr("opacity", 0)
+      .attr("r", radius)
+      .attr("cy", d => yScale(d.critic_score))
+      .attr("cx", d => xScale(d.user_score))
+      .attr("fill", "white")
+      .attr("stroke", "grey")
+      .attr("stroke-width", "0.01px")
+      .attr("stroke-opacity", 50)
+      .on('mouseover', d => {
+        div
+          .transition()
+          .duration(200)
+          .style('opacity', 0.9);
+        div
+          .html(d.game + "</br>" + d.genre + '<h3>User Score: </h3>' + d.user_score + '<h3>Critic Score: </h3>' + d.critic_score)
+          .style('top', d3.event.pageX + 'px')
+          .style('top', d3.event.pageY + 'px');
+      })
+      .on('mouseout', () => {
+        div
+          .transition()
+          .duration(500)
+          .style('opacity', 0)
+      })
+      .call(enter =>
+        enter
+        .transition()
+        .delay(d => 3 * d.user_score)
+        .duration(250)
+        .attr("fill", d => colorScale(d.user_diff))
+        .attr("opacity", 0.65)
+      ),
+
+      // update
+      update =>
+      update.call(update =>
+        update
+        .transition()
+        .delay(d => 3 * d.user_score)
+        .duration(250)
+        .attr("fill", d => colorScale(d.user_diff))
+      ),
+
+      // exit
+      exit => exit.call(exit =>
+        exit
+        .transition()
+        .delay(d => 3 * d.user_score)
+        .duration(250)
+        .attr("opacity", 0)
+        .remove()
+      ),
+
+      console.log("data joined!")
+    );
 }
