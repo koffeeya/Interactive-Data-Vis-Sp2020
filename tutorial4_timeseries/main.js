bf54bce9b2d966eddce20f1bc412c4dd90b80979
koffeeya <kavya.beheraj@gmail.com>
2020-02-22 16:33:35 -0500

    tutorial 4

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -1,81 +1,74 @@
-/* CONSTANTS AND GLOBALS */
+/* CONSTANTS & GLOBALS
+variables we will access or change in the rest of the code */
+
+// Constants
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 },
-  radius = 5;
+  margin = {
+    top: 20,
+    bottom: 50,
+    left: 60,
+    right: 40
+  };
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
+// Globals
 let svg;
 let xScale;
 let yScale;
 
-/* APPLICATION STATE */
+
+/* APPLICATION STATE
+variable to store current state of data */
 let state = {
-  data: [],
-  selection: "All", // + YOUR FILTER SELECTION
-};
-
-/* LOAD DATA */
-// + SET YOUR DATA PATH
-d3.json(YOUR_DATA_PATH, d3.autoType).then(raw_data => {
-  console.log("raw_data", raw_data);
-  state.data = raw_data;
-  init();
-});
-
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
-function init() {
-  // + SCALES
-
-  // + AXES
-
-  // + UI ELEMENT SETUP
-
-  const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-    state.selection = this.value; // + UPDATE STATE WITH YOUR SELECTED VALUE
-    console.log("new value is", this.value);
-    draw(); // re-draw the graph based on this new selection
-  });
-
-  // add in dropdown options from the unique values in the data
-  selectElement
-    .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD DATA VALUES FOR DROPDOWN
-    .join("option")
-    .attr("value", d => d)
-    .text(d => d);
-
-  // + SET SELECT ELEMENT'S DEFAULT VALUE (optional)
-
-  // + CREATE SVG ELEMENT
-
-  // + CALL AXES
-
-  draw(); // calls the draw function
+  data: []
 }
 
-/* DRAW FUNCTION */
-// we call this everytime there is an update to the data/state
+
+/* LOAD DATA 
+load data and run the init() function */
+d3.csv("../data/studentLoans.csv", d3.autotype, d => ({
+  year: d.year,
+  age: d.age_group,
+  amount: +d.amount_owed,
+  percent: d.percent_of_total,
+})).then(raw_data => {
+  console.log("raw_data", raw_data);
+  state.data = raw_data;
+});
+
+
+/* INITIALIZING FUNCTION 
+runs one time to set up scales, axes, and UI */
+function init() {
+
+  // Scales
+
+
+  // Axes
+
+
+  // UI element setup
+
+
+  // SVG setup
+
+
+  // Draw
+}
+
+/* DRAW FUNCTION
+called every time the data/state is updated */
 function draw() {
-  // + FILTER DATA BASED ON STATE
-  //
-  // + UPDATE SCALE(S), if needed
-  //
-  // + UPDATE AXIS/AXES, if needed
-  //
-  // + DRAW CIRCLES, if you decide to
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
-  //
-  // + DRAW LINE AND AREA
+
+  // Filters
+
+  // SVG: Enter, update, and exit
+  // enter
+
+
+  // update
+
+
+  // exit
+
 }
